d77c51eaa38fa2cf50f8338e09d1a7adc704af5b
sherif zayed <sherifhelmyzayed@gmail.com>
2022-11-08 00:47:12 +0200

    feat: navbar and sidebar in homepage

---
+++ script.js
@@ -0,0 +1,20 @@
+sidebar = document.getElementsByClassName('sidebar')[0];
+overlay = document.getElementsByClassName('page-overlay')[0];
+
+const openSidebar = () => {
+    sidebar.style.right = '0px';
+    sidebar.style.opacity = 1;
+    sidebar.style.zIndex = 100;
+    
+    overlay.style.opacity = 0.65;
+    overlay.style.zIndex = 50;
+}
+
+const closeSideBar = () => {
+    sidebar.style.right = '-100px';
+    sidebar.style.opacity = 0;
+    sidebar.style.zIndex = -5;
+    
+    overlay.style.opacity = 0.5;
+    overlay.style.zIndex = -50;
+}
