e59e92138bc5b270073a3e45d737a47bb1f11c52
sherif zayed <sherifhelmyzayed@gmail.com>
2022-11-13 11:20:23 +0200

    fix: fixed carousel 2 in home page

--- script.js
+++ script.js
@@ -49,3 +49,21 @@ const closeSectorCard = (id) => {
     TextEl.style.opacity = 1
     TextEl.style.zIndex = 1
 }
+
+
+// Multiple Slider
+
+// let items = document.querySelectorAll('.carousel .carousel-item')
+// 		items.forEach((el) => {
+// 			const minPerSlide = 2
+// 			let next = el.nextElementSibling
+// 			for (var i=1; i<minPerSlide; i++) {
+// 				if (!next) {
+//             // wrap carousel by using first child
+//             next = items[0]
+//         }
+//         let cloneChild = next.cloneNode(true)
+//         el.appendChild(cloneChild.children[0])
+//         next = next.nextElementSibling
+//     }
+// })
