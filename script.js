08123809d791348bc6219a6516c378d0d3a53edc
sherif zayed <sherifhelmyzayed@gmail.com>
2022-11-13 09:31:48 +0200

    style: moved sector cards js script into the main

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Sidebar Open & close functions
+
 sidebar = document.getElementsByClassName('sidebar')[0];
 overlay = document.getElementsByClassName('page-overlay')[0];
 
@@ -17,4 +19,33 @@ const closeSideBar = () => {
     
     overlay.style.opacity = 0;
     overlay.style.zIndex = -5;
+}
+
+
+
+
+
+// Sector Cards Open and close functions
+
+
+const openSectorCard = (id) => {
+    const sectorContainer = document.getElementsByClassName('project-card-sector')
+    const hiddenEl = sectorContainer[id].getElementsByClassName('project-card-text-hidden')[0]
+    hiddenEl.style.opacity = 1
+    hiddenEl.style.zIndex = 50
+    
+    const TextEl = sectorContainer[id].getElementsByClassName('project-card-text')[0]
+    TextEl.style.opacity = 0
+    TextEl.style.zIndex = -1
+}
+
+const closeSectorCard = (id) => {
+    const sectorContainer = document.getElementsByClassName('project-card-sector')
+    const hiddenEl = sectorContainer[id].getElementsByClassName('project-card-text-hidden')[0]
+    hiddenEl.style.opacity = 0
+    hiddenEl.style.zIndex = -1
+
+    const TextEl = sectorContainer[id].getElementsByClassName('project-card-text')[0]
+    TextEl.style.opacity = 1
+    TextEl.style.zIndex = 1
 }
